1affd2a9d596caad46c3ee403cf630b9d37be11b
EitanVeryKatz <eitanka@mta.ac.il>
2025-05-13 15:18:50 +0300

    InpputValidator complete

--- Ex02/InputValidator.cs
+++ Ex02/InputValidator.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace Ex02
 {
-    internal class InputValidator
+    static class InputValidator
     {
+        private static bool isValidLength(string i_input)
+        {
+            bool validLength = false;
+            if (i_input.Length == 4)
+            {
+                validLength = true;
+            }
+            return validLength;
+        }
+        private static bool containsOnlyValidChars(string i_input) {
+            bool validChars = true;
+            for(int i = 0; i < 4; i++)
+            {
+                char currentLetter = i_input.ToUpper()[i];
+                if (currentLetter < 'A' || currentLetter > 'H')
+                {
+                    validChars = false;
+                    break;
+                }
+            }
+            return validChars;
+        }
+        private static bool hasNoDuplicates(string i_input) { 
+            bool hasDuplicates = false;
+            bool[] ArrayOfLetters=new bool[8];
+            for(int i = 0;i < 4;i++)
+            {
+                char currentLetterToCheck = i_input.ToUpper()[i];
+                int indexOfLetter = currentLetterToCheck - 'A';
+                if (ArrayOfLetters[indexOfLetter] == true)
+                {
+                    hasDuplicates = true;
+                    break;
+                }
+                else
+                {
+                    ArrayOfLetters[indexOfLetter] = true;
+                }
+
+            }
+            return hasDuplicates;
+        }
+        public static bool IsValidInput(string i_input)
+        {
+            bool isValid = false;
+            if(isValidLength(i_input)&&containsOnlyValidChars(i_input)&& hasNoDuplicates(i_input))
+            {
+                isValid = true;
+            }
+            return isValid;
+
+        }
     }
 }
